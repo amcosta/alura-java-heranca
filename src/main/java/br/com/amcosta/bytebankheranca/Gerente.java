51e7b26c2556afafa68e855fabd7f7fc0c0b645f
Alex Moreno <alex.costa@finnet.com.br>
2019-10-05 11:01:11 -0300

    Construindo a classe Gerente

---
+++ src/main/java/br/com/amcosta/bytebankheranca/Gerente.java
@@ -0,0 +1,17 @@
+package br.com.amcosta.bytebankheranca;
+
+public class Gerente extends Funcionario {
+    private int senha;
+
+    public boolean autentica(int senha) {
+        return this.senha == senha;
+    }
+
+    public int getSenha() {
+        return senha;
+    }
+
+    public void setSenha(int senha) {
+        this.senha = senha;
+    }
+}
---
+++ src/test/java/br/com/amcosta/bytebankheranca/GerenteTest.java
@@ -0,0 +1,28 @@
+package br.com.amcosta.bytebankheranca;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class GerenteTest {
+
+    private Gerente construirGerente(String nome, String cpf, double salario) {
+        Gerente gerente = new Gerente();
+
+        gerente.setCpf(cpf);
+        gerente.setNome(nome);
+        gerente.setSalario(salario);
+
+        return gerente;
+    }
+
+    @Test
+    public void testVerificarGerente() {
+        Gerente gerente = this.construirGerente("Gerente 1", "12312312312", 2000);
+        Assert.assertEquals("", "Gerente 1", gerente.getNome());
+        Assert.assertEquals("", "12312312312", gerente.getCpf());
+        Assert.assertEquals("", 2000, gerente.getSalario(), 0.00001);
+
+        gerente.setSenha(123);
+        Assert.assertTrue(gerente.autentica(123));
+    }
+}
